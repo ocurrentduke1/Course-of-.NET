e9f439738ad0b5779d52e673a37fda0762998a01
ocurrentduke1 <a21110128@ceti.mx>
2025-08-06 19:30:27 -0600

    Creation of the first .NET project, with a minimalApiDemo

---
+++ Session01/MinimalApiDemo/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+SemaphoreSlim gate = new(1, 1);
+
+var app = builder.Build();
+
+app.MapPost("/createUser", (HttpContext context) =>
+{
+    var user = new { Name = "John Doe", Age = 30 };
+    return Results.Ok(user);
+});
+
+app.MapPost("/user", (HttpContext context) =>
+{
+    var user = new { Name = "John Doe", Age = 30 };
+    return Results.Created($"/user/{user.Name}", user);
+});
+
+app.MapPost("/beer", (HttpContext context) =>
+{
+    gate.Wait();
+    try
+    {
+        var requestBody = context.Request.ReadFromJsonAsync<Beer>().Result;
+        if (requestBody == null)
+        {
+            return Results.BadRequest("Invalid beer data.");
+        }
+
+        Thread.Sleep(3000); // Simulate some processing delay
+
+        var beer = new Beer(Guid.NewGuid(), requestBody.Name);
+
+        return Results.Created($"/beer/{beer.id}", beer);
+    }
+    finally
+    {
+        gate.Release();
+    }
+
+});
+
+app.Run();
+
+record Beer(Guid id, string Name);
